ec5190bf51618dcdd7999aa8df2f39d38612b942
monika <monika>
2022-10-13 15:24:50 +0545

    next button funtion not work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
-import One from './assest/one.mp4'
-import React, {useRef}   from 'react';
+import One from './assest/One.mp4'
+import two from './assest/two.mp4'
+import three from './assest/three.mp4'
+import four from './assest/four.mp4'
+import React, {useRef,useState}   from 'react';
+
 
 function App() {
-  
+  // const [count, setCount] = useState(1);
+  //  const videoList=[{id:1,src:One},
+  //    {id:2,src:two},
+  //    {id:3,src:three},
+  //   {id:4,src:four}
+  // ]
+  // let {...id}=videoList.id
+
+ 
   const videoRef =useRef();
   const handlePlay=()=>{
     videoRef.current.play();
@@ -12,18 +24,25 @@ function App() {
   const handlePause=()=>{
     videoRef.current.pause();
   }
+  // const nextBtn=()=>{
+  //   setCount()
+  // }
   return (
     <>
     <div className='container'>
     <div class="card" style={{width:'50rem'}}>
     <video preload="auto" ref={videoRef} >
-        <source src={One} type="video/mp4" />
-      </video>
+    <source src={two} type="video/mp4" />
+  </video>
+ 
   <div class="card-body">
+    <button>back</button>
    <button className='btn-play' onClick={handlePlay}>play</button>
    <button className='btn-pause' onClick={handlePause}>pause</button>
+   <button>Next</button>
   </div>
 </div>
+
     </div>
    
     </>
